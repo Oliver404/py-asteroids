952d5245db691235bb08b31820c1fc0eb9619829
Oliver Botello <47994633+Oliver404@users.noreply.github.com>
2023-10-18 16:47:30 -0600

    Changes to show asteroids, but with bug

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
-import pygame, sys, os, random, math 
+import pygame, sys, os, random, math
 from pygame.locals import *
-
+from gasteroid import GAsteroid
+# from gclases import Asteroid
+# gclases.
+# Asteroid(1,2,4,5)
 pygame.init()
 fps = pygame.time.Clock()
 
@@ -16,6 +19,7 @@ WIDTH = 800
 HEIGHT = 600
 current_speed = 7
 time = 0
+# Globals for SHIP
 ship_x = WIDTH / 2 - 50
 ship_y = HEIGHT / 2 - 50
 ship_angle = 0
@@ -24,6 +28,9 @@ ship_rotaiting = False
 ship_speed = 0
 ship_moving = False
 ship_direction = -1
+# Globals for ASTEROIDs
+asteroids = []
+asteroid_speed = 2
 
 # Canvas declaration
 window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32 )
@@ -33,6 +40,7 @@ pygame.display.set_caption('Asteroids')
 # bg = pygame.image.load(os.path.join('images', 'bg.jpg'))
 # debris = pygame.image.load(os.path.join('images', 'debris2_brown.png'))
 ship = pygame.image.load(os.path.join('assets', 'img', 'sun.png'))
+planet = pygame.image.load(os.path.join('assets', 'img', 'planet.png'))
 
 def rot_center(image, angle):
     orig_rect = image.get_rect()
@@ -50,7 +58,12 @@ def draw(canvas):
     # canvas.blit(debris, (time*.3,0))
     # canvas.blit(debris, (time*.3 - WIDTH,0))
     time = time + 1
+    
+    for i in range(0, len(asteroids)):
+        canvas.blit(rot_center(planet, asteroids[i].angle), ( asteroids[i].x, asteroids[i].y))
+
     canvas.blit(rot_center(ship, ship_angle), ( ship_x, ship_y))
+    
 
 def handle_input():
     global ship_angle, ship_rotaiting, ship_direction, ship_moving, ship_speed
@@ -77,7 +90,7 @@ def handle_input():
             elif event.key == K_UP:
                 ship_moving = False
 
-def game_logic():
+def move_ship():
     global ship_angle, ship_x, ship_y, ship_speed
     
     if (ship_rotaiting):
@@ -88,6 +101,21 @@ def game_logic():
         if ship_moving == False:
             ship_speed = ship_speed - .1
 
+def create_asteroid():
+    # pass
+    if time % 10 == 0 and time > 0:
+        ast = GAsteroid(random(0, WIDTH), random(0, HEIGHT), random(0, 365), asteroid_speed)
+        asteroids.append(ast)
+
+def move_asteroids():
+    for i in range(0, len(asteroids)):
+        asteroids[i].x = (asteroids[i].x + math.cos(math.radians(asteroids[i].angle)) * asteroids[i].speed)
+        asteroids[i].y = (asteroids[i].y - math.sin(math.radians(asteroids[i].angle)) * asteroids[i].speed)
+
+def game_logic():
+    move_ship()
+    create_asteroid()
+    move_asteroids()
 
 def update_screen():
     pygame.display.update()
---
+++ gasteroid.py
@@ -0,0 +1,6 @@
+class GAsteroid:
+    def __init__(self, x, y, angle, speed):
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.speed = speed
